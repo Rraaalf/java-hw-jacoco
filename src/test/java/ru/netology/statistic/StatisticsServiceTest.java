917325850cf4e11130a4c245bcdbdb97357f77c8
Rraaalf <kymyass@gmail.com>
2023-12-10 14:40:57 +0300

    modified test to achieve full coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,8 +9,8 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 14, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 14;
 
         long actual = service.findMax(incomesInBillions);
 
